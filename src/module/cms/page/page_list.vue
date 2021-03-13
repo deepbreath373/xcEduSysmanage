ce9398fd04caca8d4107f5a925cdec096f827428
deepbreath373 <1149735109@qq.com>
2021-03-13 21:21:55 +0800

    增加了前端条件查询功能

--- src/module/cms/page/page_list.vue
+++ src/module/cms/page/page_list.vue
@@ -1,60 +1,79 @@
 <template>
   <div>
-      <!--查询表单-->
+    <!--查询表单-->
+    <el-form :model="params">
+      <el-select v-model="params.siteId" placeholder="请选择站点">
+        <el-option
+          v-for="item in siteList"
+          :key="item.siteId"
+          :label="item.siteName"
+          :value="item.siteId">
+        </el-option>
+      </el-select>
+      页面别名：<el-input v-model="params.pageAliase" style="width: 100px"></el-input>
       <el-button type="primary" v-on:click="query" size="small">查询</el-button>
-      <!--列表-->
-      <el-table :data="list" highlight-current-row v-loading="listLoading" style="width: 100%;">
-        <el-table-column type="index" width="60">
-        </el-table-column>
-        <el-table-column prop="pageName" label="页面名称" width="120">
-        </el-table-column>
-        <el-table-column prop="pageAliase" label="别名" width="120">
-        </el-table-column>
-        <el-table-column prop="pageType" label="页面类型" width="150">
-        </el-table-column>
-        <el-table-column prop="pageWebPath" label="访问路径" width="250">
-        </el-table-column>
-        <el-table-column prop="pagePhysicalPath" label="物理路径" width="250">
-        </el-table-column>
-        <el-table-column prop="pageCreateTime" label="创建时间" width="180">
-        </el-table-column>
-      </el-table>
-        <el-pagination background layout="prev, pager, next" @current-change="changePage" :page-size="this.params.size"
-                     :total="total" :current-page="this.params.page" style="float:right;">
-      </el-pagination>
+    </el-form>
+
+    <!--列表-->
+    <el-table :data="list" highlight-current-row v-loading="listLoading" style="width: 100%;">
+      <el-table-column type="index" width="60">
+      </el-table-column>
+      <el-table-column prop="pageName" label="页面名称" width="120">
+      </el-table-column>
+      <el-table-column prop="pageAliase" label="别名" width="120">
+      </el-table-column>
+      <el-table-column prop="pageType" label="页面类型" width="150">
+      </el-table-column>
+      <el-table-column prop="pageWebPath" label="访问路径" width="250">
+      </el-table-column>
+      <el-table-column prop="pagePhysicalPath" label="物理路径" width="250">
+      </el-table-column>
+      <el-table-column prop="pageCreateTime" label="创建时间" width="180">
+      </el-table-column>
+    </el-table>
+    <el-pagination background layout="prev, pager, next" @current-change="changePage" :page-size="this.params.size"
+                   :total="total" :current-page="this.params.page" style="float:right;">
+    </el-pagination>
   </div>
 </template>
 <script>
-  import * as cmsApi from '../api/cms'
-  export default{
-    data(){
-      return {
-        list:[],
-        total:0,
-        params:{
-          page:1,
-          size:10
-        }
+import * as cmsApi from '../api/cms'
+
+export default {
+  data() {
+    return {
+      siteList: [],
+      list: [],
+      total: 0,
+      params: {
+        page: 1,
+        size: 10,
+        siteId:'',
+        pageAliase:''
       }
+    }
+  },
+  methods: {
+    query: function () {
+      //调用服务端的接口
+      cmsApi.page_list(this.params.page, this.params.size,this.params).then((res) => {
+        //将res结果数据赋值给数据模型对象
+        this.list = res.queryResult.list;
+        this.total = res.queryResult.total;
+      })
+      cmsApi.site_list().then((res) => {
+        this.siteList = res.queryResult.list;
+      })
     },
-    methods:{
-      query:function (){
-        //调用服务端的接口
-        cmsApi.page_list(this.params.page,this.params.size).then((res)=>{
-          //将res结果数据赋值给数据模型对象
-          this.list = res.queryResult.list;
-          this.total = res.queryResult.total;
-        })
-      },
-      changePage:function (page){//page形参代表当前页码
-        this.params.page = page;
-        this.query()
-      }
-    },
-    mounted() {
+    changePage: function (page) {//page形参代表当前页码
+      this.params.page = page;
       this.query()
     }
+  },
+  mounted() {
+    this.query()
   }
+}
 </script>
 <style>
 </style>
