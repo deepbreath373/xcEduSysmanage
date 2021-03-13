ef44f13d2667f5f459d1f45812c748d6a8bf7f5f
deepbreath373 <1149735109@qq.com>
2021-03-13 17:33:12 +0800

    学成在线后台管理前端

---
+++ src/module/cms/api/cms.js
@@ -0,0 +1,11 @@
+//public是对axios的工具类封装，定义了http请求方法
+import http from './../../../base/api/public'
+import querystring from 'querystring'
+let sysConfig = require('@/../config/sysConfig')
+let apiUrl = sysConfig.xcApiUrlPre;
+
+
+export const page_list = (page,size,params) => {
+  //请求服务端的页面查询接口
+  return http.requestQuickGet(apiUrl+'/cms/page/list/'+page+'/'+size)
+}
