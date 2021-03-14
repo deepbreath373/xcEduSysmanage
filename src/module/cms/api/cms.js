795a2b3f2314bd507525425730e1529215917fe2
deepbreath373 <1149735109@qq.com>
2021-03-14 11:29:32 +0800

    前端实现对cms_template的查询

--- src/module/cms/api/cms.js
+++ src/module/cms/api/cms.js
@@ -4,8 +4,22 @@ import querystring from 'querystring'
 let sysConfig = require('@/../config/sysConfig')
 let apiUrl = sysConfig.xcApiUrlPre;
 
-
+//页面查询
 export const page_list = (page,size,params) => {
+  //将params对象数据拼装成key/value串
+  let queryString = querystring.stringify(params);
   //请求服务端的页面查询接口
-  return http.requestQuickGet(apiUrl+'/cms/page/list/'+page+'/'+size)
+  return http.requestQuickGet(apiUrl+'/cms/page/list/'+page+'/'+size+"?"+queryString);
+}
+
+//站点查询
+export const site_list = () => {
+  //请求服务端的页面查询接口
+  return http.requestQuickGet(apiUrl+'/cms/site/list');
+}
+
+//模板查询
+export const template_list = () => {
+  //请求服务端的页面查询接口
+  return http.requestQuickGet(apiUrl+'/cms/template/list');
 }
